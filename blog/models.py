f76a8b8430842b4ce5903b01035ad4c810f7225d
Georgi Dimitrov <gdimitrovau@gmail.com>
2018-11-08 02:37:08 +0100

    bootstrap 0.1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+#title
+# publication date
+#body
+#image
+
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# Adding the Blog app to the settings
+# Create migration
+# migrate
+# add to the admin
