e79d60fda03b9f5f798ff167415cb0aa136045fe
Georgi Dimitrov <gdimitrovau@gmail.com>
2018-11-09 10:39:17 +0100

    added bootstrap jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
